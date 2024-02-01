655e41144f2852cc52e94b37505327029bf40a2a
stat-rupam <118969021+stat-rupam@users.noreply.github.com>
2024-02-01 15:48:05 +0530

    Update sim_model_performence_code.R

--- simulation_study/sim_model_performence_code.R
+++ simulation_study/sim_model_performence_code.R
@@ -1,15 +1,19 @@
 # Load the sys library (assuming it's not a typo, sys is not a standard R package)
 library(sys)
-K = 50
-theta = 0.5
+K = 100
+theta = 0.2
 n = 5000
-folder = "simulated_data_iii/"
-file_path_data = paste0("~/personal/spike-slab-analysis/reusable_functions/simulation_studies/",folder)
+folder_data_index = "iii" #iii,iv,v
+folder_model_index = 70 #70,50,30
+non_zero_coef_index = 1:(K*theta)
+
+file = "~/personal/spike-slab-analysis/reusable_functions/simulation_studies/"
+folder_data = paste0("simulated_data_" , folder_data_index , "/")
 file_name_data = paste0("sim_data_K_",K,"_theta_",(100*theta),".RData")
-file_path_model = "~/personal/spike-slab-analysis/reusable_functions/simulation_studies/"
 file_name_model = paste0("sp_sl_model_K_",K,"_theta_",(100*theta),".RData")
-file_name_data = paste0(file_path_data,file_name_data)
-file_name_model = paste0(file_path_model,file_name_model)
+folder_model = paste0("model_output_R_p_" , folder_model_index , "/")
+file_name_data = paste0(file,folder_data,file_name_data)
+file_name_model = paste0(file,folder_model,file_name_model)
 load(file_name_model)
 load(file_name_data)
 
@@ -36,6 +40,8 @@ y_new <- y_test
 # Initialize an empty vector for storing results
 ss <- NULL
 num_of_var <- NULL
+num_common_var <- NULL
+num_exclusive_var <- NULL
 
 # Loop through different probability thresholds
 for (prob in coef$pi[-length(coef$pi)]) {
@@ -52,17 +58,23 @@ for (prob in coef$pi[-length(coef$pi)]) {
   # Calculate R-squared for the current model
   ss_y_hat <- 1 - (sum((y_new - p_y_hat)^2) / n) / var(y_new)
   size <- dim(coef_new)[1]
+  common_var <- length(intersect(non_zero_coef_index,coef_new$index))
+  exclusive_var <- min(0,size-length(intersect(non_zero_coef_index,coef_new$index)))
+                           
   print(ss_y_hat)
   
   # Append the R-squared value to the results vector
   ss <- c(ss, ss_y_hat)
   num_of_var <- c(size, num_of_var)
+  num_exclusive_var <- c(exclusive_var,num_exclusive_var)
+  num_common_var <- c(common_var,num_common_var)
+  
 }
 
 # Create a data frame to store R-squared values and associated probabilities
 R_p <- ss
-print(data.frame(R_p_act,max(R_p),K-num_of_var[R_p==max(R_p)]))
-print(sp_sl_model$hyperparameters$w)
+print(data.frame(R_p_act,max(R_p), num_of_var[R_p==max(R_p)],
+                 num_common_var[R_p==max(R_p)],num_exclusive_var[R_p==max(R_p)]))
 prob <- coef$pi[-length(coef$pi)]
-df <- data.frame(R_p, prob, num_of_var)
-
+df <- data.frame(R_p, prob, num_of_var,num_common_var,num_exclusive_var)
+df
